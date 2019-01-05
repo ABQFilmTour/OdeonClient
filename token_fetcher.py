3d02e42ada411efd92aec4f352a1080041f4422e
Joseph Peterson <jscpeterson@gmail.com>
2019-01-04 17:33:56 -0700

    Created token_fetcher script

---
+++ token_fetcher.py
@@ -0,0 +1,8 @@
+profile = "https://www.googleapis.com/auth/userinfo.profile"
+email = "https://www.googleapis.com/auth/userinfo.email"
+openid = "https://www.googleapis.com/auth/plus.me"
+from google_auth_oauthlib.flow import InstalledAppFlow
+flow = InstalledAppFlow.from_client_secrets_file("client_secret_web.json", scopes = [profile, email, openid])
+credentials = flow.run_local_server(host = "localhost", port = 8080, open_browser = True)
+print "\n***YOUR TOKEN IS***\n"
+print credentials.id_token
