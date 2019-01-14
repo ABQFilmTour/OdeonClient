df71b01b5ae165565a3c50a23062392bf1af2f10
Joseph Peterson <jscpeterson@gmail.com>
2019-01-13 17:19:04 -0700

    Minor adjustment

--- token_fetcher.py
+++ token_fetcher.py
@@ -4,5 +4,5 @@ openid = "https://www.googleapis.com/auth/plus.me"
 from google_auth_oauthlib.flow import InstalledAppFlow
 flow = InstalledAppFlow.from_client_secrets_file("client_secret_web.json", scopes = [profile, email, openid])
 credentials = flow.run_local_server(host = "localhost", port = 8080, open_browser = True)
-print "\n***YOUR TOKEN IS***\n"
-print credentials.id_token
+print "\n***YOUR TOKEN IS***"
+print "Bearer " + credentials.id_token
