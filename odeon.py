96725cd2ff1baaa14f5e39594889fa0336c2ef37
Joseph Peterson <jscpeterson@gmail.com>
2019-01-18 22:07:28 -0700

    Token saves to clipboard, class retrieves JSON data

---
+++ odeon.py
@@ -0,0 +1,37 @@
+import token_fetcher
+import requests
+import json
+import pyperclip
+
+URL_USERS = "https://jscpeterson.com/rest/users"
+URL_LOCATIONS = "https://jscpeterson.com/rest/film_locations"
+URL_PRODUCTIONS = "https://jscpeterson.com/rest/film_productions"
+URL_COMMENTS = "https://jscpeterson.com/rest/user_comments"
+URL_IMAGES = "https://jscpeterson.com/rest/images"
+
+def getToken():
+    token = "Bearer " + token_fetcher.fetch()
+    pyperclip.copy(token) # Saves the token to the clipboard.
+    return token
+    
+def printToken():
+    print "\n***YOUR TOKEN IS***\n" + getToken()
+
+def getJson(url, token):
+    headers = {'authorization': token}
+    response = requests.get(url, headers=headers)
+    result = json.loads(response.text)
+    return result
+
+def printJson(url, token):
+    print(json.dumps(getJson(url, token), indent=4))
+
+def test():
+    token = getToken()
+    getJson(URL_LOCATIONS, pyperclip.paste())
+    getJson(URL_PRODUCTIONS, pyperclip.paste())
+    getJson(URL_COMMENTS, pyperclip.paste())
+    getJson(URL_IMAGES, pyperclip.paste())
+    printJson(URL_USERS, pyperclip.paste())
+
+print 'Done'
