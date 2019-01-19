3ed1730828259abfd23d85d9444a1df16c8db756
Joseph Peterson <jscpeterson@gmail.com>
2019-01-18 22:13:57 -0700

    Running odeon.py retrieves token, old class removed.

--- odeon.py
+++ odeon.py
@@ -9,13 +9,14 @@ URL_PRODUCTIONS = "https://jscpeterson.com/rest/film_productions"
 URL_COMMENTS = "https://jscpeterson.com/rest/user_comments"
 URL_IMAGES = "https://jscpeterson.com/rest/images"
 
+token = ""
+
 def getToken():
     token = "Bearer " + token_fetcher.fetch()
-    pyperclip.copy(token) # Saves the token to the clipboard.
     return token
     
-def printToken():
-    print "\n***YOUR TOKEN IS***\n" + getToken()
+def printToken(token):
+    print "\n***YOUR TOKEN IS***\n" + token
 
 def getJson(url, token):
     headers = {'authorization': token}
@@ -26,6 +27,7 @@ def getJson(url, token):
 def printJson(url, token):
     print(json.dumps(getJson(url, token), indent=4))
 
+"""
 def test():
     token = getToken()
     getJson(URL_LOCATIONS, pyperclip.paste())
@@ -33,5 +35,10 @@ def test():
     getJson(URL_COMMENTS, pyperclip.paste())
     getJson(URL_IMAGES, pyperclip.paste())
     printJson(URL_USERS, pyperclip.paste())
+"""  
+    
+token = getToken()
+pyperclip.copy(token) # Saves the token to the clipboard.
+printToken(token)
 
 print 'Done'
