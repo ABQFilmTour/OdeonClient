c4115486133a9706cacbcc71b2681afe2d4f9b94
Joseph Peterson <jscpeterson@gmail.com>
2019-01-19 08:59:45 -0700

    IOError provides info on Json file

--- token_fetcher.py
+++ token_fetcher.py
@@ -1,8 +1,22 @@
+from google_auth_oauthlib.flow import InstalledAppFlow
+
 def fetch():
     profile = "https://www.googleapis.com/auth/userinfo.profile"
     email = "https://www.googleapis.com/auth/userinfo.email"
     openid = "https://www.googleapis.com/auth/plus.me"
-    from google_auth_oauthlib.flow import InstalledAppFlow
-    flow = InstalledAppFlow.from_client_secrets_file("client_secret_web.json", scopes = [profile, email, openid])
+    try:
+        flow = InstalledAppFlow.from_client_secrets_file("client_secret_web.json", scopes = [profile, email, openid])
+    except IOError:
+        clientSecretNotFound()
+        return    
     credentials = flow.run_local_server(host = "localhost", port = 8080, open_browser = True)
     return credentials.id_token
+    
+def clientSecretNotFound():
+    clientIdUrl = "https://console.developers.google.com/apis/credentials?authuser=2&project=smart-athlete-221016"
+    print "Client secret file not found.\nTo authenticate this service you will need to download a client ID JSON file from the Google APIs dashboard.\nNavigate to %s\nClick Download button next to \"Web client (Auto-created for Google Sign-in)\" and download the file to this folder as \"client_secret_web.json\".\nIf you do not have access and want access, contact abqfilmtour@gmail.com." % (clientIdUrl)
+
+#handle file missing error
+    
+#socket.error: [Errno 98] Address already in use
+#   occurs if a request was opened but not closed - requires a restart, should be handled
